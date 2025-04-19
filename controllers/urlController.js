f28bdda56f053f9d726afc27e9f10eb53b15f907
AbdullahEnemy <akbuddy8@gmail.com>
2025-04-19 22:54:24 +0500

    controller:getOrignalUrl

--- controllers/urlController.js
+++ controllers/urlController.js
@@ -15,12 +15,36 @@ const createShortUrl = async (req, res) => {
     try {
         const newUrl = new Url({ url, shortCode });
         await newUrl.save();
-        res.status(201).json({
+        res.status(200).json({
             id: newUrl._id,
             url: newUrl.url,
             shortCode: newUrl.shortCode,
             createdAt: newUrl.createdAt,
-            updatedAt: newUrl.updatedAt
+            updatedAt: newUrl.updatedAt,
+            message: "Created"
+
+          });
+      } catch (err) {
+        res.status(500).json({ error: 'Failed to create short URL' });
+      }
+}
+const getOrignalUrl= async (req, res) => {
+    const url =await Url.findOne({ shortCode:req.params.shortCode });
+    if(!url){
+        return res.status(400).json({ error: 'ShortUrl not found' });
+    }
+
+    try {
+        url.accessCount++;
+        url.updatedAt=new date();
+        await url.save();
+        res.status(201).json({
+            id: url._id,
+            url: url.url,
+            shortCode: url.shortCode,
+            createdAt: url.createdAt,
+            updatedAt: url.updatedAt,
+            message: "OK"
           });
       } catch (err) {
         res.status(500).json({ error: 'Failed to create short URL' });
@@ -28,6 +52,7 @@ const createShortUrl = async (req, res) => {
 }
 const urlController = {
     createShortUrl: createShortUrl,
+    getOrignalUrl:getOrignalUrl,
 
   };
   
