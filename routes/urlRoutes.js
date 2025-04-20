4c48d37c31ecbdb8a0e7b4744033fdaf943a4dd7
AbdullahEnemy <akbuddy8@gmail.com>
2025-04-20 14:42:06 +0500

    routes:put:updateurl

--- routes/urlRoutes.js
+++ routes/urlRoutes.js
@@ -6,4 +6,5 @@ router.post('/shorten', urlController.createShortUrl);
 router.get('/shorten/:shortCode', urlController.getOriginalUrl);
 router.get('/shorten/:shortCode/stats', urlController.getUrlStatistics);
 router.delete('/shorten/:shortCode', urlController.deleteUrl);
+router.put('/shorten/:shortCode', urlController.updateUrl);
 module.exports = router;
